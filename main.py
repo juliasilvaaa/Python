68eb2c32e748ba2b26ae19cce80245b4c423dc9a
Victor Schiavo Pereira <victor.alunos@cfp127aluno.local>
2025-04-05 16:38:22 -0300

    teste

---
+++ main.py
@@ -0,0 +1,88 @@
+# # Iniciando o Python 
+# # Baixando extensão
+# # F1 - Python Create... Venv... Versão
+# # Novo arquivo main.py
+
+
+# # Atalhos 
+# # F5 =  Debugger (mais lento)
+# # CRTL + F5 = Rodar sem o debugger
+
+# print("Hello World")
+
+# # \n Para quebrar linha
+# print("Meu nome é Júlia\nTenho 18 anos")
+
+# # Concatenação = Soma/Juntar Texto
+# print("Valor do Produto:" + "R$: 58,99")
+
+# # Multiplicar a quantidade de vezes do print
+# print("Multiplicando linhas\n" * 5)
+
+# # INPUT - Faz pergunta e espera uma resposta
+# input("Qual seu nome?")
+
+# # Criar variavel para guardar - Número
+# valor = "Thau"
+
+# # Concatenação = Soma/Juntar Texto
+# print("Nome: " + valor)
+
+
+# # Criando input e retornando no print a resposta do input
+# cidade = input("Qual cidade voce mora?")
+# print("A cidade que você mora é: " + cidade)
+
+# # --------------------------------------------------------------------
+# # ----------------- Regras para nome de variavel --------------------
+# # Usar nomes significativos, evitar nomes obscuros, 
+# # Não pode começar com números, não pode ter espaços
+# # Não pode usar nome reservados (print,input do proprio python )
+# # Nomes que precisam de espaço usar _ (snake case)
+
+# nome_usuario = input("Qual seu nome?")
+
+
+# ============ Tipos de dados e operações matemáticas  ==============
+
+# --- String - Cadeia de caracteres - Texto
+# Lower () tudo minisculo 
+# Upper() tudo maisculo
+nome = "Julia"
+print(f"Seu nome é {nome.upper()}") # Maisculo
+print(nome.lower()) # Minusculo
+
+
+# --- Integer - Numeros Inteiros
+numero = 78  # Não tem aspas
+number_string = "89" # É uma string por conta das aspas
+
+# Operações Matemáticas
+print(2025 - 2006) # Subtração
+print(2 * 2) # Multiplicação
+print(2 + 6) # Soma
+print(29 / 6) # Para exibir com decimal uma barra
+print(18 // 6) # Para que ele exiba o resultado sem casa decimal utiliza duas barras
+print(25 ** 2) # Potencia ou exponenciação
+
+# Ordem = Parenteses, Potencia, Multiplicação ou divisão, Soma ou Subtração
+print( 3 * 3 + 3 / 3 - 3)
+
+print( 3 * 3 + 3 / (3 - 3)) # Se caso quiser forcar uma operação primeiro
+
+# --- Float - Numeros decimais 
+temperatura = 26.5
+
+
+# --- Boolean - Verdadeiro ou falso
+
+
+
+
+
+
+
+
+
+
+
---
+++ naruto.py
@@ -0,0 +1,10 @@
+# Desafio 1 - Seu nome ninja!
+# Contruir um gerador de nome ninja com vase em 3 perguntas 
+
+print("----- Gerador de nome ninja -----")
+
+nome = input("Qual seu nome? ")
+cidade = input("Em qual cidade voce nasceu? ")
+animal = input("Qual seu animal preferido? ")
+
+print(f"\nVocê será conhecido como {nome} da Vila oculta de {cidade}, invocador de {animal}")
